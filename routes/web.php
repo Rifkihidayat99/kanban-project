799c0b4973002d61bf0792ec5ab4f22beec56779
Rifkihidayat99 <rifkihidayatullah-student2021@smktelkom-lpg.sch.id>
2023-11-14 14:05:25 +0700

    Develop Tasklist View

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,3 +19,12 @@ Route::get('/', function () {
     return view('home'); // Diperbarui
 
 });
+
+Route::get('/', function () {
+
+    return view('home');
+
+});
+
+
+Route::get('/tasks/', [TaskController::class, 'index']);
